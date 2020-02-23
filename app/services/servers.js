056a328be3979ff5d7de21f2b2cf34cfebec7750
Jyben <benjamin.letessier@gmail.com>
2020-02-23 22:17:55 +0100

    Add singleton on the getServersList request

--- app/services/servers.js
+++ app/services/servers.js
@@ -1,12 +1,18 @@
 const Axios = require('axios');
 
 let ServersService = function () { }
+let serversList = null;
 
 ServersService.prototype.getServersList = function () {
-  return Axios({
-    url: `https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/Random/NetworkDatagramConfig.json`,
-    method: 'get'
-  });
+  if (serversList === null) {
+    serversList = Axios({
+      url: `https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/Random/NetworkDatagramConfig.json`,
+      method: 'get'
+    })
+  }
+
+  return serversList;
+
 }
 
 module.exports = ServersService;
